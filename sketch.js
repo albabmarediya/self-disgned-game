17cd61356ccd6fc3aaf71195af4e3c871e2c7312
albabmarediya <92795994+albabmarediya@users.noreply.github.com>
2022-01-27 18:03:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+var name = "albab";
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg");
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security();
+  sp = new StartPage();
+}
+
+function draw() {
+  background(bg);
+  sp.swal();
+  clues();
+  security.display();
+  
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+  }
+
+  drawSprites()
+}
---
+++ startPage.js
@@ -0,0 +1,13 @@
+class StartPage{
+    constructor(){
+        
+    }
+    swal({
+        title: `Hello! Welcome to Aladdin Hunting Treasure`,
+        text: "You reached the finish line successfully",
+        imageUrl:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-dapdm&psig=AOvVaw3IyjJzkr8hG_y35Ks2_Uys&ust=1643372325879000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDqr5D10fUCFQAAAAAdAAAAABAD",
+        imageSize: "100x100",
+        confirmButtonText: "Play"
+      });
+}
